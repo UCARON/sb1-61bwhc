c0edc5c427fc621b1a35da44e9dcbcd63d3a5935
UCARON <sontamy@gmail.com>
2024-10-07 05:43:53 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.js', // PostCSSの設定を読み込む
+  },
 })
